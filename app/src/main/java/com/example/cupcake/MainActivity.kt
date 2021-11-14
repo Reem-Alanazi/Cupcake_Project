c97b0788238eaaf3d2b766a050fcada1ba905f1c
reemalanazi <reemfawaz01@gmail.com>
2021-11-15 00:19:05 +0300

    Update title in app bar

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,27 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import androidx.appcompat.app.AppCompatActivity
+
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        //Get an instance of NavController from the NavHostFragment
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        setupActionBarWithNavController(navController)
+    }
+
+
+}
